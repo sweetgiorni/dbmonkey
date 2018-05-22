eceee23770c74edffd83dc7f0cef5e8493034863
Alex Sweet <sweet.giorni@gmail.com>
2018-05-22 10:41:55 -0700

    Style Fix
    Visual Studio Code - Beautify

--- dbmonkey.js
+++ dbmonkey.js
@@ -18,7 +18,7 @@
 var emailInquiry = {};
 var path = "";
 var root = window.location.host;
-$(function(){
+$(function () {
 
     path = window.location.pathname;
     if (path == "" || path.indexOf("index.jsp") != -1) //Home page
@@ -26,24 +26,23 @@ $(function(){
         monkeyPane = $('<div class="home_grid_block3_cell" style="background-color: #ff5959;" ><h3>DB Monkey</h3></div>').prependTo(".home_grid_divrow");
         emailInquiryButton = $('<a href="#">New Email Case</a>');
         emailInquiryComment = $('<div style="display: inline-block; vertical-align: middle;" class="ui-state-default ui-corner-all" id="q_nav1_button"> <span class="ui-icon ui-icon-comment"></span>  </div>');
-        
+
         $(monkeyPane).append(emailInquiryButton);
         $(emailInquiryButton).after(emailInquiryComment);
         $(emailInquiryComment).css({
-            "margin-left":"5px"
+            "margin-left": "5px"
         });
-        $(emailInquiryComment).click(function (){
+        $(emailInquiryComment).click(function () {
             alert('When you get a new case by email, send the "Got it" message as usual. Then, copy all the content of the "Got it" email you just sent (it contains the senders name and email). Click "New Email Case" and paste the email into the alert box. It will try and parse information from the email and apply it to a new case. Always double check the information before finalizing the case!');
-            
+
         });
-        emailInquiryButton.click(function(){
+        emailInquiryButton.click(function () {
             wdPrompt = prompt("Paste the inquiry email here.");
-            if (wdPrompt)
-            {
+            if (wdPrompt) {
                 emailInquiry.active = true;
                 //Try and find email
                 fromIndex = wdPrompt.indexOf("From:") + 6;
-                sentIndex= wdPrompt.indexOf(" Sent:");
+                sentIndex = wdPrompt.indexOf(" Sent:");
                 if (wdPrompt.indexOf("WD Inquiry") != -1)  // Check if this a WD referral
                 {
                     emailInquiry.wdReferral = true;
@@ -53,8 +52,7 @@ $(function(){
                     fromText = wdPrompt.substring(wdPrompt.indexOf("From:") + 6, wdPrompt.indexOf(" Sent:"));
                     var sepCharIndex = fromText.indexOf("<"); // Check to see if the email is enclosed in <>
                     var sepChar = "<";
-                    if (sepCharIndex == -1)
-                    {
+                    if (sepCharIndex == -1) {
                         sepCharIndex = fromText.indexOf("["); // Check to see if the email is enclosed in []
                         sepChar = "[";
                     }
@@ -72,9 +70,8 @@ $(function(){
                         }
                         else //No comma in the name
                         {
-                            spaceIndex= fullName.indexOf(' ');
-                            if (spaceIndex != -1)
-                            {
+                            spaceIndex = fullName.indexOf(' ');
+                            if (spaceIndex != -1) {
                                 firstName = fullName.substring(0, spaceIndex);
                                 lastName = fullName.substring(spaceIndex, fullName.length);
                             }
@@ -83,15 +80,13 @@ $(function(){
                         lastName = $.trim(lastName);
                         emailInquiry.firstName = firstName;
                         emailInquiry.lastName = lastName;
-                    } 
+                    }
                     email = fromText.substring(sepCharIndex + (sepChar == "<" ? 1 : 8), fromText.length - 1);
-                    if (email)
-                    {
+                    if (email) {
                         emailInquiry.email = email;
                     }
 
-                    if (email == fullName)
-                    {
+                    if (email == fullName) {
                         emailInquiry.firstName = "";
                         emailInquiry.lastName = "";
                     }
@@ -100,8 +95,8 @@ $(function(){
                     emailInquiry.scenario = scenario;
 
                 }
-                
-                
+
+
                 GM_setValue("emailInquiry", emailInquiry);
                 window.location.href = ("/add_client_1.jsp");
             }
@@ -112,16 +107,14 @@ $(function(){
     else if (path.indexOf("add_client") != -1) //Add client page
     {
         emailInquiry = GM_getValue("emailInquiry");
-        if (emailInquiry != undefined && emailInquiry.active)
-        {
+        if (emailInquiry != undefined && emailInquiry.active) {
             $("country_input").val("USA").trigger("change"); // Set country to USA by default
             $("#client_type_input").val("individual").trigger("change");  //Change client type
             if (emailInquiry.firstName && emailInquiry.lastName) //Add client's name 
             {
                 $("#company_input").val(emailInquiry.lastName + ", " + emailInquiry.firstName).trigger("change");
             }
-            else
-            {
+            else {
                 $("#company_input").val("need, need").trigger("change");
             }
 
@@ -131,22 +124,21 @@ $(function(){
             {
                 $("#email_input").val(emailInquiry.email).trigger("change");
             }
-            else
-            {
+            else {
                 $("#email_input").val("need@need.need").trigger("change");
             }
-            
+
             $("#city_input").val("need").trigger("change");  // Set the client's city
             $("#state_input").val("APO - AA").trigger("change"); //Set the client's state
-            
+
             $("#refer_by_input").val((emailInquiry.wdReferral ? "Western Digital" : "Google")).trigger("change");  // Set the referred by value
-            
+
             emailInquiry.active = false;
             GM_setValue("emailInquiry", emailInquiry);
         }
     }
 
-    else if (path.indexOf("new_case" ) != -1) //Add case page
+    else if (path.indexOf("new_case") != -1) //Add case page
     {
         $("#case_setup_method").val("email").trigger("change");  // Change the setup method
         //Service type will default to Standard Hard Drive
