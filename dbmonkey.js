53dc4e2005183c3b60d8c165b7ce9545fc7acb19
Michael <Michael@DESKTOP-V8EHIV7>
2019-06-10 10:15:13 -0500

    Initial Ransomware Email adjusted

--- dbmonkey.js
+++ dbmonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DB Monkey
 // @namespace    https://db.datarecovery.com
-// @version      0.41
+// @version      0.42
 // @description  DB quality of life improvements! https://github.com/sweetgiorni/dbmonkey
 // @author       Alex Sweet
 // @match        https://db.datarecovery.com/*
@@ -23,7 +23,7 @@
 
 var versionUpdateInfo = ""+
         "<ul>"+
-        "    <li>Initial Ransomware Email adjusted</li>"+
+        "    <li>Adjusted discount to 30% in WD initial contact letter, added text about free 2TB drive to both initial contact letters</li>"+
         "</ul>";
 
 
@@ -40,9 +40,9 @@ We received your online data recovery request, and I'm here to answer your quest
             
 You should have received a case setup letter, which includes a case ID number, shipping instructions, and our contact information. Note that we provide free expedited shipping labels to get your case here quickly. This label will come in a separate email from UPS or FedEx.
             
-After we receive and evaluate your case, we'll provide a detailed analysis with an estimated turnaround time, chance of recovery, and a price quote. Data recovery costs start around $200, but vary considerably depending on the complexity of the case. 
+After we receive and evaluate your case, we'll provide a detailed analysis with an estimated turnaround time, chance of recovery, and a price quote. Data recovery costs start around $200 but vary considerably depending on the complexity of the case. 
             
-You're under no obligation to proceed with recovery after receiving the quote. If you decline our recovery services, we will ship back your drive at our expense. If you approve our quote, you will only pay if the recovery is successful. 
+You're under no obligation to proceed with recovery after receiving the quote. If you decline our recovery services, we will ship back your drive at our expense. If you approve our quote, you will only pay if the recovery is successful. Also, we currently offer a free 2TB external drive with a successful recovery. 
             
 To finalize your case setup, please email me at this address or call me at the number listed below to provide some more information. I can also answer any questions you have about the case process or our recovery capabilities.  
             
@@ -56,7 +56,7 @@ You should have received a case setup letter, which includes a case ID number, s
             
 After we receive and evaluate your case, we'll provide a detailed analysis with an estimated turnaround time, chance of recovery, and a price quote. Data recovery costs start around $200, but vary considerably depending on the complexity of the case. 
             
-As a referral from Western Digital, you'll receive free shipping both ways and a 10% discount off the recovery quote.
+As a referral from Western Digital, you'll receive free shipping both ways, a free 2TB external drive with a successful recovery, and a 30% discount off the recovery quote.
             
 You're under no obligation to proceed with recovery after receiving the quote. If you decline our recovery services, we will ship back your drive at our expense. If you approve our quote, you will only pay if the recovery is successful. 
             
